b75cf1014e8c8c69970b3df8c165c20499f82efb
Alison Golden <alisong779@yahoo.com>
2019-03-22 02:47:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,21 +10,6 @@ elseif phrase == "what did you eat today?".upcase
 elseif phrase == "what".upcase
   puts "no, not since 1938!".upcase
 end
+end
 
 
-
-
-
-# Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
