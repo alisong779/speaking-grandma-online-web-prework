661129481af86a2d8303e9f633f67ebbd3e0a15e
Alison Golden <alisong779@yahoo.com>
2019-03-22 02:57:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elseif phrase == "Hi Nana, how are you?"
+elsif phrase == "Hi Nana, how are you?"
   puts "huh?! speak up, sonny!".upcase
-elseif phrase == "Hi!"
+elsif phrase == "Hi!"
   puts "huh?! speak up, sonny!".upcase
-elseif phrase == "what did you eat today?".upcase
+elsif phrase == "what did you eat today?".upcase
   puts "no, not since 1938!".upcase
-elseif phrase == "what".upcase
+elsif phrase == "what".upcase
   puts "no, not since 1938!".upcase
 else
   puts "huh?! speak up, sonny!".upcase
