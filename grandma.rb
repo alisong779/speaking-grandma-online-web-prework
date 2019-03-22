434491aea62781a8683f001964fee24c5da9c1c1
Alison Golden <alisong779@yahoo.com>
2019-03-22 02:44:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,14 @@ if phrase == "I love you Grandma!".upcase
   puts "I love you too pumpkin".upcase
 elseif phrase == "Hi Nana, how are you?"
   puts "huh?! speak up, sonny!".upcase
-elseif phrase == ""
+elseif phrase == "Hi!"
+  puts "huh?! speak up, sonny!".upcase
+elseif phrase == "what did you eat today?".upcase
+  puts "no, not since 1938!".upcase
+elseif phrase == "what".upcase
+  puts "no, not since 1938!".upcase
+end
+
 
 
 
