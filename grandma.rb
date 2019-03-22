7b54ac145b2552ec6f9c3f35bde9b960a40c7995
Alison Golden <alisong779@yahoo.com>
2019-03-22 02:58:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "Hi Nana, how are you?"
   puts "huh?! speak up, sonny!".upcase
 elsif phrase == "Hi!"
